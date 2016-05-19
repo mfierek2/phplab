e6de772073a29f41d017ccd2debdb9297991d5f8
Michał Fierek <mfierek@openmailbox.org>
2016-05-19 23:16:57 +0200

    some update

--- zad1l10.php
+++ zad1l10.php
@@ -19,26 +19,27 @@
 	<div id="container">
 		<div class="tile53">
 			<?php
-				session_start();
-				$counter_name = "counter.txt";
-				if (!file_exists($counter_name)) {
-				  $f = fopen($counter_name, "w");
-				  fwrite($f,"0");
-				  fclose($f);
-				}
+				if(!isset($_COOKIE['visits'])) {
+					error_reporting(0);
+					echo '<span style="font-size: 26px;">'.'Jesteś odwiedzającym nr '.$counterVal .'</span>';
+					setcookie('visits','diskfdsljfclksmi',time()+86400);
+					$counter_name = "counter.txt";
+					if (!file_exists($counter_name)) {
+						$f = fopen($counter_name, "w");
+						fwrite($f,"0");
+						fclose($f);
+					}
 
-				$f = fopen($counter_name,"r");
-				$counterVal = fread($f, filesize($counter_name));
-				fclose($f);
-
-				if(!isset($_SESSION['hasVisited'])){
-				  $_SESSION['hasVisited']="yes";
-				  $counterVal++;
-				  $f = fopen($counter_name, "w");
-				  fwrite($f, $counterVal);
-				  fclose($f); 
+					$f = fopen($counter_name,"r");
+					$counterVal = fread($f, filesize($counter_name));
+					fclose($f);
+					$counterVal++;
+					$f = fopen($counter_name, "w");
+					fwrite($f, $counterVal);
+					fclose($f); 
 				}
-				echo '<span style="font-size: 26px;">'.'Jesteś odwiedzającym nr '.$counterVal .'</span>';	
+				else
+					echo "Dziś już tu byłeś"
 			?>
 		</div>
 	</div>
