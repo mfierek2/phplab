9b10160745ed7c981c84f7b8f7fc4c37b2f8a48e
sUser <sUser>
2016-05-19 21:30:25 +0200

    roboczy update

---
+++ zad1l10.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+	<title>Michał Fierek</title>
+
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="css/fontello.css" type="text/css" />
+	<link href='http://fonts.googleapis.com/css?family=Lato|Josefin+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+	<script src="timer.js"></script>
+
+</head>
+
+<body onload="odliczanie();">
+
+	<div id="container">
+		<div class="tile53">
+			<?php
+				session_start();
+				$counter_name = "counter.txt";
+				if (!file_exists($counter_name)) {
+				  $f = fopen($counter_name, "w");
+				  fwrite($f,"0");
+				  fclose($f);
+				}
+
+				$f = fopen($counter_name,"r");
+				$counterVal = fread($f, filesize($counter_name));
+				fclose($f);
+
+				if(!isset($_SESSION['hasVisited'])){
+				  $_SESSION['hasVisited']="yes";
+				  $counterVal++;
+				  $f = fopen($counter_name, "w");
+				  fwrite($f, $counterVal);
+				  fclose($f); 
+				}
+				echo '<span style="font-size: 26px;">'.'Jesteś odwiedzającym nr '.$counterVal .'</span>';	
+			?>
+		</div>
+	</div>
+</body>
+</html>
